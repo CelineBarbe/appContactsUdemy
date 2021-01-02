c409516c41c102977c0a99d51a55c59c94f2f4cd
Céline Barbe <laloutrecosmique@hotmail.fr>
2021-01-02 14:30:03 +0100

    first commit

---
+++ src/composants/Liste.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import Contact from './Contact'
+
+export default class Liste extends Component {
+
+    state = {
+        contacts : [
+            {
+                id: 1,
+                nom: 'John Doe',
+                email: 'john@gmail.com',
+                tel: '555-555-555'
+            },
+            {
+                id: 2,
+                nom: 'John Doe',
+                email: 'john@gmail.com',
+                tel: '555-555-555'
+            },
+            {
+                id: 3,
+                nom: 'John Doe',
+                email: 'john@gmail.com',
+                tel: '555-555-555'
+            }
+        ]
+    }
+
+    supprime = (id) => {
+        const nvContacts = this.state.contacts.filter(contact => 
+            contact.id !== id)
+
+            this.setState({
+                contacts: nvContacts
+            })
+    
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.contacts.map(contact => (
+                    <Contact 
+                    key={contact.id}
+                    nom={contact.nom}
+                    email={contact.email}
+                    tel={contact.tel}
+                    supprimeClick={() => this.supprime(contact.id)}/>
+                ))}
+            </div>
+        )
+    }
+}
